72be8b2bed909817cfb7fdf976c9a69b5d69df92
tejas-gojariya <tejasgojariya036@gmail.com>
2024-04-21 16:38:07 +0530

    Create a custome hook to fetch the games

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,19 @@
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+
+const GameGrid = () => {
+  const { games, error } = useGames();
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
