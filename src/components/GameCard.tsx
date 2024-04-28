0b72e3acf9d8d7a36c2814452edb55b637168970
tejas-gojariya <tejasgojariya036@gmail.com>
2024-04-28 09:33:45 +0530

    Build a card Component and fix in grid

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,19 @@
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading>{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
