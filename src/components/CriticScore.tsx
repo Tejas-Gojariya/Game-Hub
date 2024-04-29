d58254a627919b5fe95527e239e7b741db83cb9f
tejas-gojariya <tejasgojariya036@gmail.com>
2024-04-29 09:18:43 +0530

    Displaying sritic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius={4}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
