8538b1c01c695f0bafbc27fd54ef41da06d2a341
tejas-gojariya <tejasgojariya036@gmail.com>
2024-05-01 18:31:30 +0530

    Add usePlatforms hook - fetching platform data

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,8 @@
+import { useQueries, useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+import { Platform } from "./useGames";
 
 interface platforms {
   id: number;
@@ -6,6 +10,15 @@ interface platforms {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
