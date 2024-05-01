1af34a25ff5676b224d5226ee45c33cfeca21444
tejas-gojariya <tejasgojariya036@gmail.com>
2024-05-01 18:14:25 +0530

    react-query :  for data fetching and caching

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { ChakraBaseProvider, ColorModeScript } from "@chakra-ui/react";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import {
+  ReactQueryDevtools,
+  ReactQueryDevtoolsPanel,
+} from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import theme from "./theme.ts";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraBaseProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraBaseProvider>
   </React.StrictMode>
 );
